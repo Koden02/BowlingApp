6e5a938da52790b6d66f390ecc66deeb8251c6fc
Koden02 <koden_kabau@yahoo.com>
2023-10-09 06:06:34 -0400

    Added Axios package and fixed menu issue.

--- BowlingApp/ClientApp/src/components/Bowling.js
+++ BowlingApp/ClientApp/src/components/Bowling.js
@@ -3,19 +3,36 @@ import axios from 'axios';
 
 const BowlingGame = () => {
     const [diceRollResult, setDiceRollResult] = useState(0);
+    const [pinsKnockedDown, setPinsKnockedDown] = useState(null);
 
     const rollDice = () => {
         const randomNumber = Math.floor(Math.random() * 20) + 1;
         setDiceRollResult(randomNumber);
 
         // Take this random rolled number and send it to the API for it to decide what the result is.
-        axios.post('/api/calculateScore', { randomNumber })
+        axios.post('/api/bowling/calculateScore', { rollNumber: randomNumber })
             .then(response => {
-                // Add the code to update the UI here with the result here.
+                // Handle the response and update the UI here with the result
+                const pinsDowned = response.data.pinsKnockedDown;
+                setPinsKnockedDown(pinsDowned);
+                // Update the UI
             })
             .catch(error => {
                 // Handle/display errors
+                console.error('Error: ', error);
+            });
+    };
 
+    const startNewGame = () => {
+        // Send a request to start a new game
+        axios.post('/api/bowling/newGame')
+            .then(response => {
+                // Handle the response and update the UI
+                console.log(response.data.message); // Log the server response
+            })
+            .catch(error => {
+                // Handle/display errors
+                console.error('Error:', error);
             });
     };
 
@@ -23,7 +40,9 @@ const BowlingGame = () => {
         <div>
             <h1>Bowling App</h1>
             <button onClick={rollDice}>Roll the ball!</button>
+            <button onClick={startNewGame}>Start New Game</button>
             {diceRollResult && <p>Dice Roll Result: {diceRollResult}</p>}
+            {pinsKnockedDown !== null && <p>Pins Knocked Down: {pinsKnockedDown}</p>}
             {/* Everything else goes here or above */}
         </div>
     );
